b9b7620978731f1b826367e080f7cb15428a532e
Tolomatico <ballesty.t@gmail.com>
2023-06-19 00:31:42 -0300

    1er commit

--- App.js
+++ App.js
@@ -4,17 +4,52 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header} >
+        <Text style={styles.headerText} >Header</Text>
+      </View>
+      <View style={styles.main}>
+        <Text style={styles.text}>Hola coder!!!</Text>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+
   },
+  header: {
+
+    flex: 0.1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "indigo"
+
+
+
+  },
+  headerText: {
+    color: "white",
+    fontWeight: "bold"
+
+
+  }
+
+  ,
+  main:{
+    flex:0.9,
+    justifyContent:'center',
+    alignItems:"center"
+
+
+  },
+
+  text: {
+    color: "indigo",
+  }
 });
